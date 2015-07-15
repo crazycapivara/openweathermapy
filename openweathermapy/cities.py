b9f32526be447805fe7a26421a4bf35fa277d3cf
kuethe <kuethe@saopaulo>
2015-07-16 00:00:20 +0200

    cities.py updated

--- openweathermapy/cities.py
+++ openweathermapy/cities.py
@@ -2,9 +2,10 @@
 """
 	openweathermapy.cities
 	~~~~~~~~~~~~~~~~~~~~~~
-	This Module contains functions to retrieve information (mostly `id`,
-	`latitude` and `longitude`) for cities supported by *openweathermap.org*.
-
+	The cities module contains functions to retrieve information (mostly id,
+	latitude and longitude) for cities supported by *OpenWeatherMap.org*.
+	You can search for cities by name.	
+	
 	:copyright: (c) 2015 by Stefan Kuethe.
 	:license: GPLv3, see <http://www.gnu.org/licenses/gpl.txt> for more details.
 """
@@ -14,6 +15,7 @@ __author__ = "Stefan Kuethe"
 __license__ = "GPLv3"
 
 URL_CITY_LIST = "http://openweathermap.org/help/city_list.txt"
+"""url, where to fetch current city list"""
 
 class CityData(object):
 	"""List of cities.
@@ -25,8 +27,8 @@ class CityData(object):
 	   * longitude (lon),
 	   * country code (countryCode)
 	"""
-	def __init__(self, data):
-		data = [tuple(line.split("\t")) for line in data.splitlines()]
+	def __init__(self, data, separator="\t"):
+		data = [tuple(line.split(separator)) for line in data.splitlines()]
 		self.keys = data.pop(0)
 		self.data = data
 
@@ -39,10 +41,11 @@ class CityData(object):
 		return self.data
 
 	def get(self, city):
-		"""Search for `city` and return list of matches.
-
-		`city` must either be city name or comma-separated
-		city name with country code, e. g. `New York` or `New York,US`.
+		"""Search for ``city`` and return list of matches.
+		
+		Args:
+		   city (str): city must either be city name or comma-separated
+		   city name with country code, e. g. `New York` or `New York,US`.
 		"""
 		try:
 			name, country = city.split(",")
@@ -51,19 +54,19 @@ class CityData(object):
 		data = [line for line in self.data if line[1].find(name) >= 0 and line[-1].find(country.upper()) >= 0]
 		return data
 
-	def get_dicts(self, city):
-		"""Same as `get`, but matches are returned as dictionaries."""
+	def get_dict(self, city):
+		"""Same as ``get`` method, but matches are returned as dictionaries."""
 		data = self.get(city)
 		data_dict = [dict(zip(self.keys, line)) for line in data]
 		return data_dict
 
 def load_cities():
-	"""Load city data from *OpenWeatherMap.org* and return `CityData` object."""
+	"""Load city data from *OpenWeatherMap.org* and return ``CityData`` object."""
 	data = utils.get_url_response(URL_CITY_LIST)
 	return CityData(data)
 
 def load_cities_from_file(filename):
-	"""Load city data from file and return `CityData` object."""
+	"""Load city data from file and return ``CityData`` object."""
 	with file(filename) as f:
 		data = f.read()
 	return CityData(data)
